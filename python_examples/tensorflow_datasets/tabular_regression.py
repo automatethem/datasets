1f0a4329d443f36e8bf69cb6f9b399abcc6bb8ca
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2025-04-07 23:04:55 +0900

    Add files via upload

---
+++ python_examples/tensorflow_datasets/tabular_regression.py
@@ -0,0 +1,5 @@
+
+#!pip install tensorflow_datasets
+
+#https://soundprovider.tistory.com/entry/Tensorflow-tfds%EB%A5%BC-%ED%99%9C%EC%9A%A9%ED%95%9C-custom-dataset-%EC%83%9D%EC%84%B1
+#https://velog.io/@vivilsh1226/Tensorflow-custom-dataset-%EB%A7%8C%EB%93%A4%EA%B8%B0
---
+++ python_examples/torch.utils.data+datasets/tabular_regression.py
@@ -0,0 +1,29 @@
+#pip install datasets
+#pip install pytorch
+
+from datasets import load_dataset
+import torch
+
+raw_dataset_dict = load_dataset("csv", data_files={"train": "custom-datasets/basic/tabular_regression/father_son_height/train.csv", "validation": "custom-datasets/basic/tabular_regression/father_son_height/validation.csv"})
+raw_train_dataset = raw_dataset_dict['train']
+raw_validation_dataset = raw_dataset_dict['validation']
+
+class SonHeightDataset(torch.utils.data.Dataset):
+    def __init__(self, raw_dataset):
+        self.raw_dataset = raw_dataset
+
+    def __len__(self):
+        return len(self.raw_dataset)
+
+    def __getitem__(self, index):
+        example = self.raw_dataset[index]
+        return example['Father'], example['Son']
+
+train_dataset = SonHeightDataset(raw_train_dataset)
+validation_dataset = SonHeightDataset(raw_validation_dataset)
+
+for example in train_dataset:
+    Father, Son = example
+    print(Father) #165.1
+    print(Son) #151.892
+    break
